c15e9aabf2a2f6b6fe38034912e94b1e3c7a9180
Nathan da Silva <pacsmacck@gmail.com>
2021-07-28 02:45:32 -0700

    server js setup for apollo server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
