69b14cd1079bd735693eb023981268602d63fac1
Nathan da Silva <pacsmacck@gmail.com>
2021-07-28 03:24:17 -0700

    thought definition

--- server/server.js
+++ server/server.js
@@ -15,11 +15,16 @@ const server = new ApolloServer({
   resolvers
 })
 
+// integrate our apollo server with the express application as middleware
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 });
