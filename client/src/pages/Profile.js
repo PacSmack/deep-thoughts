844c18cf98cf8e2676e4eb2a6e9cf327e718c0b5
Nathan da Silva <pacsmacck@gmail.com>
2021-07-30 07:36:55 -0700

    profile.js page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
 
 const Profile = () => {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam }
+  });
+
+  const user = data?.user || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div>
       <div className="flex-row mb-3">
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
-  
+
   const { loading, data } = useQuery(QUERY_THOUGHT, {
     variables: { id: thoughtId }
   })
 
   const thought = data?.thought || {};
 
-  if(loading) {
+  if (loading) {
     return <div>Loading...</div>
   }
 
@@ -29,6 +30,8 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
